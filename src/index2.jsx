880bfc0643c7076336d6bfe679661bba1814c299
wangxuecong <aa940209@163.com>
2018-06-11 19:45:29 +0800

    更新webpack配置文件使之支持多入口文件

---
+++ src/index2.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+// const App = require('./components/App.jsx');
+
+ReactDOM.render(<App />, document.getElementById('root'));
