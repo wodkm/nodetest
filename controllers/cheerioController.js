711a722376def0aa7324b3891cffd5a6ff0e3a46
wangxuecong <aa940209@163.com>
2018-10-23 20:07:21 +0800

    同步request，测试url为简书url

--- controllers/cheerioController.js
+++ controllers/cheerioController.js
@@ -5,20 +5,6 @@ const request = require('request');
 const util = require('util');
 const getPromise = util.promisify(request.get);
 
-// router.get("/cheerio", async ctx => {
-//     var news = new Array();
-//     await util.promisify(request.get)('https://www.jianshu.com/').then((err, res) => {
-//         if (err) return console.error(err);
-//         var $ = cheerio.load(res.body.toString());
-//         var table = $('.note-list');
-//         console.log(table);
-//         console.log(ctx);
-//         ctx.response.body = 123;
-//     }).catch((err) => {
-//         console.log(err.message);
-//     });
-// });
-
 router.get("/cheerio", async ctx => {
     let result = await getPromise('https://www.jianshu.com/', {
         'auth': {
@@ -28,8 +14,8 @@ router.get("/cheerio", async ctx => {
         }
     });
     // 可以加入 try catch 捕获异常  也可以加 .catch()
-    console.log("result", result);
-    ctx.response.body = result;
+    console.log("result", result.body);
+    ctx.response.body = result.body;
 });
 
 module.exports = router.routes();
