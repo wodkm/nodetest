41981a35b7965e213c14977271445ab2cb51ebbe
Wodkm <aa940209@163.com>
2019-07-23 20:34:11 +0800

    redis操作

--- controllers/cheerioController.ts
+++ controllers/cheerioController.ts
@@ -24,19 +24,25 @@ const remote_get = function (url: string) {
 myRouter.get("/cheerio_imgUrl", async ctx => {
     let result = await remote_get('https://www.jianshu.com/');
     let $ = cheerio.load(result.text);
-    // 可以加入 try catch 捕获异常  也可以加 .catch()
-    let img_url: any[] = [];
     $('li').each((index: any, item: any) => {
-        img_url.push($(item).find('img').attr('src'));
+        //此处将爬下来的图片URL存入redis当中，只是爬虫和redis联动的一次尝试
         try {
             if ($(item).find('img').attr('src')) {
-                redisUtil.lpush('cheerio_imgUrl', $(item).find('img').attr('src'));
+                //此处使用redis中的set，避免重复数据
+                redisUtil.sadd('cheerio_imgUrl', $(item).find('img').attr('src'));
             }
         } catch (error) {
             logger.error(error);
         }
     });
-    ctx.response.body = img_url.join('\n');
+    redisUtil.multi()
+        .scard("cheerio_imgUrl")
+        .smembers("cheerio_imgUrl").keys("*", function (err, replies) {
+        });
+    // redisUtil.smembers('cheerio_imgUrl', data => {
+    //     console.log(data);
+    //     ctx.response.body = data;
+    // });
 });
 
 myRouter.get("/cheerio_html", async ctx => {
