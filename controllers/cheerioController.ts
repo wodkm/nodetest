83375712075038a5a3339973348a79ae6af94842
wangxcy <wangxcy@yonyou.com>
2020-04-08 18:04:28 +0800

    更新引用版本

--- controllers/cheerioController.ts
+++ controllers/cheerioController.ts
@@ -22,23 +22,23 @@ const remote_get = function (url: string) {
 }
 
 myRouter.get("/cheerio_imgUrl", async ctx => {
-    let result = await remote_get('https://www.jianshu.com/');
-    let $ = cheerio.load(result.text);
-    $('li').each((index: any, item: any) => {
-        //此处将爬下来的图片URL存入redis当中，只是爬虫和redis联动的一次尝试
-        try {
-            if ($(item).find('img').attr('src')) {
-                //此处使用redis中的set，避免重复数据
-                redisUtil.sadd('cheerio_imgUrl', $(item).find('img').attr('src'));
-            }
-        } catch (error) {
-            logger.error(error);
-        }
-    });
-    redisUtil.multi()
-        .scard("cheerio_imgUrl")
-        .smembers("cheerio_imgUrl").keys("*", function (err, replies) {
-        });
+    // let result = await remote_get('https://www.jianshu.com/');
+    // let $ = cheerio.load(result.text);
+    // $('li').each((index: any, item: any) => {
+    //     //此处将爬下来的图片URL存入redis当中，只是爬虫和redis联动的一次尝试
+    //     try {
+    //         if ($(item).find('img').attr('src')) {
+    //             //此处使用redis中的set，避免重复数据
+    //             redisUtil.sadd('cheerio_imgUrl', $(item).find('img').attr('src'));
+    //         }
+    //     } catch (error) {
+    //         logger.error(error);
+    //     }
+    // });
+    // redisUtil.multi()
+    //     .scard("cheerio_imgUrl")
+    //     .smembers("cheerio_imgUrl").keys("*", function (err, replies) {
+    //     });
     // redisUtil.smembers('cheerio_imgUrl', data => {
     //     console.log(data);
     //     ctx.response.body = data;
